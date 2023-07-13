57d0b3a8740a93ee886c995623887ea0e2bbd64c
Tyler Pruitt <tylerp@creatrify.com>
2023-07-13 16:58:24 -0700

    Added .env file and updated config initialization accordingly

--- Robinhood Crypto/main.py
+++ Robinhood Crypto/main.py
@@ -1,6 +1,7 @@
 from bot import SpotGridTradingBot
 from order import *
 from error_queue import ErrorQueue, ErrorQueueLimitExceededError
+from dotenv import dotenv_values
 
 def confirm_grids(upper_price, lower_price, level_num, cash):
     print("Please confirm you want the following:")
@@ -28,33 +29,38 @@ def confirm_grids(upper_price, lower_price, level_num, cash):
         return False
 
 if __name__ == '__main__':
+    env_config = dotenv_values(".env")
+    
     config = {
-        'crypto': 'LTC',
-        'days_to_run': 7,
-        'mode': 'test',
+        'crypto': env_config['CRYPTO'],
+        'days_to_run': int(env_config['DAYS_TO_RUN']),
+        'mode': env_config['MODE'],
         'backtest': {
-            'interval': '',
-            'span': '',
-            'bounds': '',
+            'interval': env_config['BACKTEST_INTERVAL'],
+            'span': env_config['BACKTEST_SPAN'],
+            'bounds': env_config['BACKTEST_BOUNDS'],
         },
-        'upper_price': 91.5,
-        'lower_price': 89,
-        'level_num': 5,
-        'cash': 30,
-        'loss_threshold': 5,
-        'loss_percentage': 25,
-        'latency_in_sec': 60,
-        'is_static': False,
-        'send_to_discord': True,
-        'discord_latency_in_hours': 0.25,
-        'discord_url': 'https://discord.com/api/webhooks/1114803095394861096/lMqJldCxv4nOEan2QWl9TeIfJErGiUUZ9G_ujTj7pKq57tu9VpsdKawOCtkCzEF-cX8d',
-        'max_error_count': 20,
-        'error_latency_in_sec': 30,
-        'init_buy_error_latency_in_sec': 30,
-        'init_buy_error_max_count': 5,
-        'cancel_orders_upon_exit': 'buy'
+        'upper_price': float(env_config['UPPER_PRICE']),
+        'lower_price': float(env_config['LOWER_PRICE']),
+        'level_num': int(env_config['LEVEL_NUM']),
+        'cash': float(env_config['CASH']),
+        'loss_threshold': float(env_config['LOSS_THRESHOLD']),
+        'loss_percentage': float(env_config['LOSS_PERCENTAGE']),
+        'latency_in_sec': float(env_config['LATENCY_IN_SEC']),
+        'is_static': bool(env_config['IS_STATIC']),
+        'send_to_discord': bool(env_config['SEND_TO_DISCORD']),
+        'discord_latency_in_hours': float(env_config['DISCORD_LATENCY_IN_HOURS']),
+        'discord_url': env_config['DISCORD_URL'],
+        'max_error_count': int(env_config['MAX_ERROR_COUNT']),
+        'error_latency_in_sec': float(env_config['ERROR_LATENCY_IN_SEC']),
+        'init_buy_error_latency_in_sec': float(env_config['INIT_BUY_ERROR_LATENCY_IN_SEC']),
+        'init_buy_error_max_count': int(env_config['INIT_BUY_ERROR_MAX_COUNT']),
+        'cancel_orders_upon_exit': env_config['CANCEL_ORDERS_UPON_EXIT']
     }
     
     if confirm_grids(config['upper_price'], config['lower_price'], config['level_num'], config['cash']):
         spot_grid_trader = SpotGridTradingBot(config)
+        del env_config, config
         spot_grid_trader.run()
+    else:
+        del env_config, config
